b45b07eb3028f171ad7ee2984874d1bdadd74975
GGO-web <iicandyoii@gmail.com>
2023-01-24 23:24:26 +0200

    Removed trip relationship from snaps model (user can always view snaps)

--- src/models/Snap.model.ts
+++ src/models/Snap.model.ts
@@ -2,13 +2,11 @@ import { Document, model, Schema } from "mongoose";
 
 export interface ISnap extends Document {
   userId: string; 
-  tripId: string;
   image: string; // base64 image format
 }
 
 const snapSchema = new Schema<ISnap>({
   userId: { type: String, required: true },
-  tripId: { type: String },
   image: { type: String, required: true },
 });
 
