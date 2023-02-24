69e0aed8363698025b89d0eee640394d8faf6368
GGO-web <iicandyoii@gmail.com>
2023-02-25 00:01:58 +0200

    Removed controller for feedback and moved routes registering directly into router

--- dist/features/feedback/feedback.router.js
+++ dist/features/feedback/feedback.router.js
@@ -1,35 +1,52 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const asyncWrapper_1 = require("../../helpers/asyncWrapper");
-const feedback_controller_1 = __importDefault(require("./feedback.controller"));
+const getDirectoryFiles_1 = require("../../helpers/getDirectoryFiles");
+const path_1 = __importDefault(require("path"));
 const router = express_1.default.Router();
-setTimeout(() => {
-    // Notification routes
-    feedback_controller_1.default.forEach((routeConfig) => {
-        const method = routeConfig.method;
-        switch (method) {
-            case "get":
-                router.get(routeConfig.path, (0, asyncWrapper_1.asyncWrapper)(routeConfig.route));
-                break;
-            case "post":
-                router.post(routeConfig.path, (0, asyncWrapper_1.asyncWrapper)(routeConfig.route));
-                break;
-            case "put":
-                router.put(routeConfig.path, (0, asyncWrapper_1.asyncWrapper)(routeConfig.route));
-                break;
-            case "delete":
-                router.delete(routeConfig.path, (0, asyncWrapper_1.asyncWrapper)(routeConfig.route));
-                break;
-            case "patch":
-                router.patch(routeConfig.path, (0, asyncWrapper_1.asyncWrapper)(routeConfig.route));
-                break;
-            default:
-                break;
-        }
-    });
-}, 0);
+// register all routes
+(0, getDirectoryFiles_1.getDirectoryFiles)(path_1.default.join(__dirname, "./routes")).map((routeName) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    const route = yield (_a = `./routes/${routeName}`, Promise.resolve().then(() => __importStar(require(_a))));
+    const routeConfig = route.default;
+    const method = routeConfig.method;
+    // @ts-ignore
+    router[method](routeConfig.path, (0, asyncWrapper_1.asyncWrapper)(routeConfig.route));
+}));
 exports.default = router;
--- src/features/feedback/feedback.router.ts
+++ src/features/feedback/feedback.router.ts
@@ -1,36 +1,29 @@
 import express, { Router } from "express";
 
 import { asyncWrapper } from "../../helpers/asyncWrapper";
+import { getDirectoryFiles } from "../../helpers/getDirectoryFiles";
 
-import controller, { IRouteConfig } from "./feedback.controller";
+import path from "path";
 
 const router: Router = express.Router();
 
-setTimeout(() => {
-  // Notification routes
-  controller.forEach((routeConfig: IRouteConfig) => {
-    const method = routeConfig.method;
+export interface IRouteConfig {
+  route: any;
+  method: string;
+  path: string;
+}
 
-    switch (method) {
-      case "get":
-        router.get(routeConfig.path, asyncWrapper(routeConfig.route));
-        break;
-      case "post":
-        router.post(routeConfig.path, asyncWrapper(routeConfig.route));
-        break;
-      case "put":
-        router.put(routeConfig.path, asyncWrapper(routeConfig.route));
-        break;
-      case "delete":
-        router.delete(routeConfig.path, asyncWrapper(routeConfig.route));
-        break;
-      case "patch":
-        router.patch(routeConfig.path, asyncWrapper(routeConfig.route));
-        break;
-      default:
-        break;
-    }
-  });
-}, 0);
+// register all routes
+getDirectoryFiles(path.join(__dirname, "./routes")).map(async (
+  routeName: string
+) => {
+  const route = await import(`./routes/${routeName}`);
+  const routeConfig: IRouteConfig = route.default;
+  const method = routeConfig.method;
+
+  // @ts-ignore
+  router[method](routeConfig.path, asyncWrapper(routeConfig.route));
+});
+
 
 export default router;
